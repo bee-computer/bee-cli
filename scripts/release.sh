0593f164c8992286a0185d135a923a78afe2ef85
Steve Korshakov <korshaks@amazon.com>
2026-02-04 13:23:49 -0800

    chore: use bun version in release

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,7 +17,7 @@ bun install
 bun run typecheck
 bun run build
 
-npm version "$VERSION" -m "chore(release): %s"
+bun version "$VERSION" -m "chore(release): %s"
 
 bun publish --access public
 git push --follow-tags
